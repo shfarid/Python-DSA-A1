f6ac3e85b3d0fccfa4328819a3e9e4d321e1eb17
shfarid <127935064+shfarid@users.noreply.github.com>
2025-08-12 15:44:58 +0200

    Add files via upload

---
+++ Lesson-1/print.py
@@ -0,0 +1,29 @@
+
+'''
+
+print('Python', end='-')
+print('C++')
+print('Java')
+'''
+'''
+
+print('Python', 'Java', 'C++', sep = '\n')
+
+
+print('Hi', 'Jo', sep = '\n' )
+
+a, b, c = 1, 2, 3
+print(a, b, c)
+
+far = 1981
+ism = 2011
+print('before: ', far, ism)
+
+ism, far = far, ism
+print('nach: ', far, ism)
+
+'''
+
+# Data types
+x = 0
+print(x, type(x))
