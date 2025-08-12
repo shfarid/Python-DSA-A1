0a232ef324e7339b50b0fc3b91744ff5c3871db1
shfarid <127935064+shfarid@users.noreply.github.com>
2025-08-12 16:58:22 +0200

    Add files via upload

---
+++ Lesson-1/variables.py
@@ -0,0 +1,67 @@
+# Variables - are containers for storing data values
+# Python has no command for declaring a variable, unlike some programming languages like Java, C++
+#Variables do not need to be declared with any particular type,
+# and can even change type after they have been set.
+x = 5  # x is integer / int
+x = 'Mark'  # x is string / str
+123
+#Casting
+#If we want to specify the data type of a variable, this can be done with casting:
+x = str(3)  # x will be '3' str
+y = int(3)  # y will be 3 int
+z = float(3)    # z will be 3.0 float
+
+# Get the Type
+# You can get the data type of a variable with the type() function.
+
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+
+# Case-Sensitive / Variable names are case-sensitive.
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+# Variables in Python are dynamic
+x = 5   #can be set as int
+x = 'Bob'     #then as str
+
+# Variable names
+# 1. Must start with a letter or underscore:
+name = 'John'
+_names = ['John', 'Bob']
+# 2. Cannot start with a number
+2_names = ['John', 'Bob']   # 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
