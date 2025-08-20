52a3110d8f8f90e4811bcd7ec9b94ec6aed083f9
Farid Shafiyev <faridshafiyev@gmail.com>
2025-08-20 22:15:35 +0200

    homeworks update

---
+++ HomeWorks/3.py
@@ -0,0 +1,22 @@
+'''
+color = input('insert color please: ')
+
+match color.lower():
+  case 'red':
+    print("Stop")
+  case 'yellow':
+    print('Ready')
+  case 'green':
+    print('Go')
+'''
+
+
+'''
+user_name = input()
+names = ['Aysel', 'Elvin', 'Nigar']
+
+if user_name.capitalize() in names:
+    print('My dear friend')
+else:
+    print('I do not know you')
+'''
